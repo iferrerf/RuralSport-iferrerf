36d6a81c70527672a9d999b4591a5e4f5533f089
Mario <mpariciob@gmail.com>
2023-04-27 22:03:47 +0200

    Añadido inicio de sesión con roles

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_app_final_iferrerf/pages/adminpage.dart';
+
 import 'pages/imports.dart';
 import 'pages/pages.dart';
 
@@ -26,17 +28,44 @@ class MyApp extends StatelessWidget {
         ),
         nextScreen: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              //El usuario ya está logueado
-              return Drawer_Menu();
+           builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // caso base mientras se espera la respuesta del Future
+              return Scaffold(body: Center(child: CircularProgressIndicator()));
+            } else if (snapshot.hasData) {
+  // el usuario ya está logueado
+  return FutureBuilder<DocumentSnapshot>(
+    future: FirebaseFirestore.instance.collection('roles').doc(snapshot.data!.uid).get(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        // caso base mientras se espera la respuesta del Future
+        return Scaffold(body: Center(child: CircularProgressIndicator()));
+      } else if (snapshot.hasData && snapshot.data!.exists) {
+        // Se añade una comprobación adicional aquí para asegurarse de que el documento realmente existe
+        Map<String, dynamic>? data = snapshot.data!.data() as Map<String, dynamic>?;
+        if (data != null && data.containsKey('role')) {
+          // Comprueba si el campo 'role' existe antes de intentar acceder a él
+          if (data['role'] == 'admin') {
+                      return AdminPage();
+                    } else {
+                      return Drawer_Menu();
+                    }
+                  } else {
+                    return Drawer_Menu();
+                  }} else {
+                    return Drawer_Menu();
+                  }
+                },
+              );
             } else {
-              //El usuario no está logueado
+              // el usuario no está logueado
               return Login();
             }
+
           },
         ),
         splashTransition: SplashTransition.rotationTransition,
+        duration: 2000,
       ),
     );
   }
